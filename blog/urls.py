f15553e757240fcecf071909aba02e09ceba67b8
aoulaa <olimbek.nizomov95@gmail.com>
2022-01-11 22:57:45 +0500

    model view serializer requirements gitignore admin

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog.views import PostListView, PostDetailView, vote_view, CommentListView,
 urlpatterns = [
     path('post', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/vote/', vote_view, name='vote_view'),
+    path('post/<int:pk>/vote/', vote_view, name='vote-view'),
     path('comment', CommentListView.as_view(), name='comment-list'),
     path('comment/<int:pk>', CommentDetailView.as_view(), name='comment-detail'),
 
