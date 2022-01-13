419599eb537d942a1da8f10d09398b43d7dd6a88
aoulaa <olimbek.nizomov95@gmail.com>
2022-01-14 02:45:50 +0500

    test

--- newsApp/settings.py
+++ newsApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 0)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS.extend(
     filter(
         None,
